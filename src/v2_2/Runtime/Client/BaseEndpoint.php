8997167f00c6ea2f939aac95b9f4810ef43fb4a3
sebprt <sebastien@kiboko.fr>
2022-09-23 10:47:45 +0200

    Fixed query parameters

--- src/v2_2/Runtime/Client/BaseEndpoint.php
+++ src/v2_2/Runtime/Client/BaseEndpoint.php
@@ -22,11 +22,7 @@ abstract class BaseEndpoint implements Endpoint
     }
     public function getQueryString(): string
     {
-        $optionsResolved = $this->getQueryOptionsResolver()->resolve($this->queryParameters);
-        $optionsResolved = array_map(function ($value) {
-            return null !== $value ? $value : '';
-        }, $optionsResolved);
-        return http_build_query($optionsResolved, '', '&', PHP_QUERY_RFC3986);
+        return http_build_query($this->queryParameters, '', '&', PHP_QUERY_RFC3986);
     }
     public function getHeaders(array $baseHeaders = []): array
     {
